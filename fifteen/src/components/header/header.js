d3f0ea3a05c1a4356665728c9440addf1e303eea
yossogood8611 <rooproop1111@naver.com>
2021-03-25 17:41:48 +0900

    [feat] file 위치 수정
    css, img file 추가

--- fifteen/src/components/header/header.js
+++ fifteen/src/components/header/header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import styles from './header.module.css';
+import styles from '../../../public/css/header.module.css';
 
 class Header extends Component {
     render() {
