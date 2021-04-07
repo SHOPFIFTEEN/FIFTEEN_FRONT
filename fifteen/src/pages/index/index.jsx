0d6c4b7372cf25fd026b7015dacea93ac98a9942
yossogood8611 <rooproop1111@naver.com>
2021-04-08 02:39:22 +0900

    [feat] add header banner
    미안 실수로 브랜치 파는거 까먹고 main 에서 작업함
    근데 다시 브랜치 파서 하기 귀찮으니까 그냥 푸시할게 ㅎㅎ
    하나 둘 셋 소공 파이팅!~
    (사실 sidenav css도 한 줄 만졌어.)

--- fifteen/src/pages/index/index.jsx
+++ fifteen/src/pages/index/index.jsx
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import Sidenav from "../../components/sidenav/Sidenav";
+import Banner_1 from '../../img/banner.svg';
+import Banner_2 from '../../img/banner_2.svg';
+import './index.css';
 
 class Index extends Component {
     render() {
@@ -9,6 +12,8 @@ class Index extends Component {
             <div>
                 <Header/>
                 <Sidenav/>
+                <img src={Banner_1} className="indexImg"/>
+                <img src={Banner_2} className="indexImg"/>
                 <Footer/>
             </div>
         );
