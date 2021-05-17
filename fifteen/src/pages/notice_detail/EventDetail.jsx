01303c56e70830b046ac6b4107dae8aa1d7b636a
yui880 <kimyuna521@gmail.com>
2021-05-17 16:40:32 +0900

    [feat] notice, event 상세페이지

---
+++ fifteen/src/pages/notice_detail/EventDetail.jsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+import '../notice/notice_page.css';
+import '../order/orderPage.css';
+import './NoticeDetail.css'
+import Header from '../../../src/components/header/Header';
+import Footer from '../../components/footer/Footer';
+import {Link, withRouter} from "react-router-dom";
+
+
+
+class EventDetail extends Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <div className="orderPage">
+                    <div className="orderPage_line" />
+                    <div className="orderPage__title">Event</div>
+                    <div className="orderPage__main">
+
+                        <div className="noticePage-nav">
+                            <div className="noticePage-nav-side">
+                                <Link to='/notice'><div className="noticePage-nav-text">공지</div></Link>
+                                <Link to='/event'><div className="noticePage-nav-text">이벤트</div></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='order_info_title'>이벤트</div>
+                            <div className='notice-info'>
+                                <div className='notice_info_box'>
+                                    <div className='notice-info-box-titleBox'>
+                                        <div className='notice-info-box-titleBox-title'>제목</div>
+                                        <div className='notice-info-box-titleBox-text'>이벤트 테스트</div>
+                                    </div>
+                                    <div className='notice-info-box-subBox'>
+                                        <div className='notice-info-box-subBox-date'>
+                                            <div className='notice-info-box-subBox-title'>작성일</div>
+                                            <div className='notice-info-box-subBox-text'>2021-05-17</div>
+                                        </div>
+                                        <div className='notice-info-box-subBox-views'>
+                                            <div className='notice-info-box-subBox-title'>조회수</div>
+                                            <div className='notice-info-box-subBox-text'> </div>
+                                        </div>
+                                    </div>
+                                    <div className='notice-info-box-main'>
+
+                                    </div>
+                                    <button className='notice-info-box-btn'>목록으로</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <Footer/>
+            </div>
+        )
+    }
+}
+export default withRouter(EventDetail);
