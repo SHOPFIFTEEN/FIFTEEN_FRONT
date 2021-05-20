a03d2e6b969cba053695aa15f0d1e1e1df09642a
yossogood8611 <rooproop1111@naver.com>
2021-05-20 15:40:12 +0900

    [feat] 경로에 따른 product 상세 페이지 위의 브레드크럼 추가
    - sort 추가된거 검색 페이지에도 적용

--- fifteen/src/components/product/product.jsx
+++ fifteen/src/components/product/product.jsx
@@ -8,82 +8,90 @@ import Header from '../../../src/components/header/Header';
 import Footer from '../../../src/components/footer/Footer';
 import {getCookie} from "../../cookies";
 
-class Product extends Component{
+class Product extends Component {
 
-        constructor(props) {
-            super(props);
-            this.state = {
-                productInfo: {productSeq : 12},
-                count : 0,
-                productSeq : ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            productInfo: {productSeq: 12},
+            count: 0,
+            productSeq: '',
+            keyword : 'field'
+        }
+    }
+
+    getProductInfoList = async function () {
+        let result = await axios({
+            method: 'GET',
+            url: `http://52.79.196.94:3001/product/${this.props.match.params.productSeq}`,
+            data: {},
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            params: {
+                productSeq: this.props.match.params.productSeq
             }
-        }
+        })
+        this.setState({productInfo: result.data[0]})
+    }
 
-        getProductInfoList = async function () {
-            let result =await axios ({
-                method : 'GET',
-                url : `http://52.79.196.94:3001/product/${this.props.match.params.productSeq}`,
-                data: { },
-                headers : {
-                    "Content-Type" : 'application/json'
-                },
-                params : {
-                    productSeq : this.props.match.params.productSeq
-                }
-            })
-            this.setState({productInfo: result.data[0]})
-        }
+    addCart = () => {
+        let result = axios({
+            method: 'POST',
+            url: `http://52.79.196.94:3001/cart/add/${this.props.match.params.productSeq}`,
+            headers: {
+                "Content-Type": 'application/json',
+                "x-access-token": getCookie("accessToken"),
+            },
+            data: {
+                count: this.state.count
+            }
+        }).then((result) => {
+            if (result.status < 400) {
+                alert('담았습니다.')
+            }
+        })
+    }
 
-        addCart = () => {
-            let result =axios ({
-                method : 'POST',
-                url : `http://52.79.196.94:3001/cart/add/${this.props.match.params.productSeq}`,
-                headers : {
-                    "Content-Type" : 'application/json',
-                    "x-access-token" : getCookie("accessToken"),
-                },
-                data : {
-                    count : this.state.count
-                }
-            }).then((result)=>{
-                if(result.status<400){
-                    alert('담았습니다.')
-                }
-            })
-        }
+    plusCount = () => {
+        var c = this.state.count;
+        this.setState({count: ++c});
+    }
+    minusCount = () => {
+        var c = this.state.count;
+        this.setState({count: --c});
+    }
 
-        plusCount= () =>{
-            var c = this.state.count;
-            this.setState({count : ++c});
-        }
-        minusCount =()=> {
-            var c = this.state.count;
-            this.setState({count : --c});
-        }
+    componentDidMount() {
+        this.setState({keyword : this.props.match.params.keyword})
+        this.getProductInfoList();
+    }
 
-        componentDidMount() {
-            this.getProductInfoList();
-        }
-
-    render(){
-        return(
+    render() {
+        return (
             <div className="productPage">
                 <Header/>
                 <div className="ProductPage-main">
                     <div className="product">
+                        {!(this.state.keyword === 'field') ?
+                            <div className='breadCrumb'>'{this.props.match.params.keyword}' 검색 > {this.state.productInfo.title}</div> :
+                            <div className='breadCrumb'>{this.state.productInfo.field} > {this.state.productInfo.title}</div>}
                         <div className="product-main">
                             <div className="product-main-imgBox">
                                 <img className="product-main-imgBox-img" src={this.state.productInfo.image}/></div>
                             <div className="product-main-box">
                                 <div className="product-main-box-category">{this.state.productInfo.field}</div>
                                 <div className="product-main-box-title">{this.state.productInfo.title}</div>
-                                <div className="product-main-box-author">지은이 : {this.state.productInfo.author} / 출판사 : {this.state.productInfo.publisher}</div>
+                                <div className="product-main-box-author">지은이 : {this.state.productInfo.author} / 출판사
+                                    : {this.state.productInfo.publisher}</div>
                                 <div className="product-main-box-comment">{this.state.productInfo.a_intro}</div>
                                 <div className="product-main-box-price-total">Total</div>
                                 <div className="product-main-box-price">{this.state.productInfo.price}원</div>
                                 <div className="product-main-box-button">
                                     <button className="product-main-box-button-buy">BUY NOW</button>
-                                    <button className="product-main-box-button-cart" onClick={this.addCart}>ADD TO CART</button>
+                                    <button className="product-main-box-button-cart" onClick={this.addCart}>ADD TO
+                                        CART
+                                    </button>
                                     <div>
                                         {this.state.productInfo.title} | {this.state.productInfo.author}
                                         <div onClick={this.plusCount}>+</div>
@@ -114,21 +122,21 @@ class Product extends Component{
                                     <div className="product-detail-box-information-box">
                                         <div className="product-detail-box-information-title">배송 방법</div>
                                         -상품 수령 후 7일 이내에 상품에 하자가 있을 시에 사용전에 고객센터로 연락주셔야만 교환 및 환불이 가능합니다. <br/>
-                                        -고객센터에 반품요청을 하시거나, Q&A 게시판을 통해 반품요청해주세요. <br />
-                                        -고객님의 사용 또는 과실로 의해 상품의 가치가 감소한 경우에는 교환/반품이 불가능합니다.<br />
+                                        -고객센터에 반품요청을 하시거나, Q&A 게시판을 통해 반품요청해주세요. <br/>
+                                        -고객님의 사용 또는 과실로 의해 상품의 가치가 감소한 경우에는 교환/반품이 불가능합니다.<br/>
                                         -상품에 대한 반품/교환 신청 절차 없이 임의로 택배를 반송 하거나, 배송지 착오로 인해 분실 된 경우에는 책임지지 않습니다.<br/>
-                                        <br />
+                                        <br/>
                                         (자세한 내용은 고객센터를 이용해 주시기 바랍니다.)<br/>
-                                        <br />
-                                        -상품 하자 이외 단순변심에 의한 교환, 반품의 경우 반송비는 고객님께서 부담하셔야 합니다. (색상 교환, 사이즈 교환 등 포함)<br />
-                                        -교환/반품비는 제품에 따라 상이 하오니 이점 유의하시기 바랍니다.<br />
+                                        <br/>
+                                        -상품 하자 이외 단순변심에 의한 교환, 반품의 경우 반송비는 고객님께서 부담하셔야 합니다. (색상 교환, 사이즈 교환 등 포함)<br/>
+                                        -교환/반품비는 제품에 따라 상이 하오니 이점 유의하시기 바랍니다.<br/>
                                     </div>
                                 </div>
                                 <div className="product-detail-box-review">
-                                    <Review />
+                                    <Review/>
                                 </div>
                                 <div className="product-detail-box-qna">
-                                    <QnA />
+                                    <QnA/>
                                 </div>
                             </div>
                         </div>
