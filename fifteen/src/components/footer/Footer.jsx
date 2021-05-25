63c92366af1b16d13210689964fe51795b0c5b89
yui880 <kimyuna521@gmail.com>
2021-05-26 00:18:39 +0900

    [feat] privacy policy, agreement

--- fifteen/src/components/footer/Footer.jsx
+++ fifteen/src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import styles from './footer.module.css';
-import {withRouter} from "react-router-dom";
+import {withRouter, Link} from "react-router-dom";
 
 class Footer extends Component {
     render() {
@@ -8,21 +8,31 @@ class Footer extends Component {
             <div>
                 <div className={styles.footer}>
                     <div className={styles.footer__box}>
+                        <div className={styles.footer__box_customer}>
+                            <div className={styles.footer__box_customer__title}>customer</div>
+                            <div className={styles.footer__box_customerText}>
+                                <div>(주) MAPASHOP</div>
+                                <div>대표 이사 : 김애웅</div>
+                                <div>FAX : 02-7890-4132</div>
+                                <div>사업자 등록 : 0142-91-8591</div>
+                            </div>
+                        </div>
+                        <div className={styles.footer__box_row}>
                         <div className={styles.footer__box_follow}>
                             <div className={styles.footer__box_follow__title}>follow</div>
                             <div className={styles.footer__box_followText}>
                                 <div>instagram</div> <div> kakao 1:1</div>
                             </div>
                          </div>
-                        <div className={styles.footer__box_customer}>
-                            <div className={styles.footer__box_customer__title}>customer</div>
-                            <div className={styles.footer__box_customerText}>
-                                 <div>(주) MAPASHOP</div>
-                                 <div>대표 이사 : 김애웅</div>
-                                 <div>FAX : 02-7890-4132</div>
-                                 <div>사업자 등록 : 0142-91-8591</div>
+                        <div className={styles.footer__box_info}>
+                            <div className={styles.footer__box_follow__title}>Info</div>
+                            <div className={styles.footer__box_followText}>
+                                <Link to='/privacy'> <div>PrivacyPolicy</div> </Link>
+                                <Link to='/agreement'><div> Agreement</div></Link>
                             </div>
                         </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
